5c8604933938712bf7c8440a17eeeede0589a11b
Piotr Krzeminski <piotr@krzeminski.it>
2017-12-30 13:05:04 +0100

    Print the root DIV on Command+G

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ exports.decorateTerm = (Term, { React, notify }) => {
     constructor (props, context) {
       super(props, context);
 
+      this._onTerminal = this._onTerminal.bind(this);
+
       globalShortcut.register('CommandOrControl+G', () => {
         console.log('CommandOrControl+G pressed');
+        console.log('Root div:');
+        console.log(this._rootDiv);
       })
     }
 
     render () {
-      return React.createElement(Term, this.props);
+      return React.createElement(Term, Object.assign({}, this.props, {
+        onTerminal: this._onTerminal
+      }));
+    }
+
+    _onTerminal (term) {
+      if (this.props.onTerminal) {
+        this.props.onTerminal(term);
+      }
+
+      this._rootDiv = term.div_;
     }
   }
 };
